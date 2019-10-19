2f23adc52614371981d729c10be18988ba70df68
nelsoncarmogit <nelsoncarmo@gmail.com>
2019-10-19 16:40:47 -0300

    modulo 01

--- curso/modulo01/index.js
+++ curso/modulo01/index.js
@@ -4,6 +4,7 @@ const server = express();
 
 server.use(express.json());
 
+// 
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "nome": "Nelson", "email": "nelsoncarmo@gmail.com"}
